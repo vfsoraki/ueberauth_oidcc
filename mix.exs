44e349992b400050f826a2147839deebc31f4f71
Paul Swartz <paul@paulswartz.net>
2023-11-21 20:43:55 -0500

    chore: update version to `-rc`
    You can't publish a package to Hex with a pre-release dependency unless
    the package itself is also pre-release.`

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule UeberauthOidcc.MixProject do
   def project do
     [
       app: :ueberauth_oidcc,
-      version: "0.1.0",
+      version: "0.1.0-rc.0",
       elixir: "~> 1.15",
       description: """
       An Ueberauth strategy for generic OpenID Connect authentication.
