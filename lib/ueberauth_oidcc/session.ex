f0724206d3c2973102d8707c6987ce6db6aff27f
Paul Swartz <paul@paulswartz.net>
2023-12-29 22:45:34 -0500

    feat(session): better enforcement of cookie security

--- lib/ueberauth_oidcc/session.ex
+++ lib/ueberauth_oidcc/session.ex
@@ -15,12 +15,18 @@ defmodule UeberauthOidcc.Session do
         max_age: opts.session_max_age
       )
 
-    Conn.put_resp_cookie(conn, opts.session_cookie, value, cookie_opts(opts))
+    Conn.put_resp_cookie(
+      conn,
+      cookie_name(conn.scheme, opts.session_cookie),
+      value,
+      cookie_opts(opts)
+    )
   end
 
   @spec get(Conn.t(), Config.t(), default :: term()) :: term()
   def get(conn, opts, default \\ %{}) do
-    with cookie when is_binary(cookie) <- conn.req_cookies[opts.session_cookie],
+    with cookie when is_binary(cookie) <-
+           conn.req_cookies[cookie_name(conn.scheme, opts.session_cookie)],
          {:ok, session} <-
            Plug.Crypto.decrypt(conn.secret_key_base, opts.session_key, cookie,
              max_age: opts.session_max_age
@@ -32,7 +38,22 @@ defmodule UeberauthOidcc.Session do
   end
 
   def delete(conn, opts) do
-    Conn.delete_resp_cookie(conn, opts.session_cookie, cookie_opts(opts))
+    Conn.delete_resp_cookie(
+      conn,
+      cookie_name(conn.scheme, opts.session_cookie),
+      cookie_opts(opts)
+    )
+  end
+
+  defp cookie_name(:https, session_cookie) do
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#attributes
+    # > Cookies with names starting with __Secure- (dash is part of the prefix)
+    # must be set with the secure flag from a secure page (HTTPS).
+    "__Secure-" <> session_cookie
+  end
+
+  defp cookie_name(_conn, session_cookie) do
+    session_cookie
   end
 
   defp cookie_opts(opts) do
